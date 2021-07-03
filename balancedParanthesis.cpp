c4e7b38f35f62d599dc28a0a952b13600861da1b
Harsh Prasad <explorerharsh@gmail.com>
2021-07-03 12:35:11 +0530

    Balanced Paranthesis solution using Stacks

---
+++ balancedParanthesis.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <string>
+#include <stack>
+
+using namespace std;
+
+bool isValid(string s){
+    int n = s.size();
+    stack<int> st;
+    bool ans = true;
+    
+    for(int i=0; i<n; i++){
+        if(s[i] == '(' or s[i] == '{' or s[i] == '['){
+            st.push(s[i]);
+        }
+        else if(s[i] == ')'){
+            if(!st.empty() and st.top() == '('){
+                st.pop();
+            } else {
+                ans = false;
+                break;
+            }
+        }
+        else if(s[i] == '}'){
+            if(!st.empty() and st.top() == '{'){
+                st.pop();
+            } else {
+                ans = false;
+                break;
+            }
+        }
+        else if(s[i] == ']'){
+            if(!st.empty() and st.top() == '['){
+                st.pop();
+            } else {
+                ans = false;
+                break;
+            }
+        }
+    }
+    
+    if(!st.empty()){
+        return false;
+    }
+    
+    return ans;
+}
+
+
+int main()
+{
+    string s = "[{()}]";
+    
+    if(isValid(s)){
+        cout << "Vaild String" << endl;
+    } else {
+        cout << "Invalid String" << endl;
+    }
+
+    return 0;
+}
